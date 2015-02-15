bce69315c33b4f7a4d6bfb55af0b37afa54c71ac
Waine Tam <wainetam@Waines-MacBook-Air.local>
2015-02-15 17:33:24 -0500

    init commit

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Waine Tam on 2/15/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
